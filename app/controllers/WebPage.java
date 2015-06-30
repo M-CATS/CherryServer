4b9c8783ff92360edf5be2bfe9f18b7d634c939a
AwfulOrange <602270434@qq.com>
2015-06-30 16:10:24 +0800

    update sth about Newsinfo page

--- app/controllers/WebPage.java
+++ app/controllers/WebPage.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import java.util.*;
+import models.Comment;
 import models.News;
+import models.NewsDetail;
 import play.*;
 import play.mvc.*;
 import views.html.*;
@@ -9,8 +12,9 @@ public class WebPage extends Controller {
 
     public static Result news(Long newsid) {
     	
-    	News n=News.findByid(newsid);
-        return ok(NewsInfo.render(n));
+    	NewsDetail n=NewsDetail.findByid(newsid);
+    	List<Comment> c=Comment.findByNewsid(newsid);
+        return ok(NewsInfo.render(n,c));
     }
 
 }
